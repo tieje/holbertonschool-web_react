233e15a36913d84c53ab680eccd5455f73d62c72
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-05-18 09:42:56 -0400

    wc: work in progress

---
+++ 0x02-react_intro/task_4/dashboard/src/App.test.js
@@ -0,0 +1,22 @@
+//Enzyme is dead. I'm going to use React Testing Library
+//https://dev.to/wojtekmaj/enzyme-is-dead-now-what-ekl
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from "./App";
+
+test('<App /> renders', () => {
+    render(<App />)
+    expect(screen.getByText('School dashboard')).toBeInTheDocument()
+})
+test('<App /> renders div with class App-header', () => {
+    render(<App />)
+    expect(screen.getByTestId('App-header')).toBeInTheDocument()
+})
+test('<App /> renders div with class App-body', () => {
+    render(<App />)
+    expect(screen.getByTestId('App-body')).toBeInTheDocument()
+})
+test('<App /> renders div with class App-footer', () => {
+    render(<App />)
+    expect(screen.getByTestId('App-footer')).toBeInTheDocument()
+})
---
+++ 0x02-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,32 @@
+import './Notifications.css'
+import { getLatestNotification } from './utils'
+const Notifications = () => {
+    function handleClick() {
+        console.log('Close button has been clicked')
+    }
+    return (
+        <div className='Notifications grid gap-4 border border-dashed border-red-500 p-5 m-5'>
+            <div className='grid grid-cols-1 justify-items-end'>
+                <button
+                    className='w-10 text-lg hover:bg-gray-200 rounded-lg'
+                    onClick={handleClick}
+                    aria-label='Close'
+                >X</button>
+            </div>
+            <p>
+                Here is the list of notifications
+            </p>
+            <ul className='grid gap-1 pl-10'>
+                <li className='text-blue-700' data-priority='default' data-testid='notification'>
+                    New course available
+                </li>
+                <li className='text-red-700' data-priority='urgent' data-testid='notification'>
+                    New course available
+                </li>
+                <li className='text-red-500' dangerouslySetInnerHTML={{ __html: getLatestNotification() }} data-testid='notification'>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Notifications

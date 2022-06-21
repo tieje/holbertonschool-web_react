6bbbb3b429478b5122239a9ea3a275b371f2ce6b
tieje <toj320@gmail.com>
2022-06-21 09:45:51 -0400

    wc: work in progress

---
+++ 0x07-React_Immutable/6-deeply.js
@@ -0,0 +1,7 @@
+const { Map } = require('immutable')
+function mergeDeeplyElements(page1, page2) {
+    const map1 = Map(page1)
+    const map2 = Map(page2)
+    return (map1.mergeDeep(map2))
+}
+module.exports = { mergeDeeplyElements }

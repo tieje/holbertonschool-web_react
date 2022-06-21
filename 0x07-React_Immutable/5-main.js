a5fb09c394571d00f66284aae23a6d494caab78d
tieje <toj320@gmail.com>
2022-06-21 09:38:09 -0400

    wc: work in progress

--- 0x07-React_Immutable/5-main.js
+++ 0x07-React_Immutable/5-main.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+const { concatElements, mergeElements } = require('./5-merge.js')
+const a = [1, 2, 3]
+const b = [4, 5, 6]
+const greeting = {
+    hello: 1,
+    hey: 2,
+    hi: 3,
+}
+const names = {
+    tj: 1,
+    m: 3,
+    k: 4
+}
+console.log(concatElements(a, b))
+console.log(mergeElements(greeting, names))

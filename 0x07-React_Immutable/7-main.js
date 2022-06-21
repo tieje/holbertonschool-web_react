6aeb023c6fe62057dd2e04e58a9670f667c9212a
tieje <toj320@gmail.com>
2022-06-21 09:51:33 -0400

    wc: work in progress

--- 0x07-React_Immutable/7-main.js
+++ 0x07-React_Immutable/7-main.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+const { areMapsEqual } = require('./7-equality.js')
+const { Map } = require('immutable')
+const map1 = new Map(
+    {
+        firstName: 'Guillaume',
+        lastName: 'Salva',
+    }
+);
+const map2 = new Map(
+    {
+        firstName: 'Guillaume',
+        lastName: 'Salva',
+    }
+);
+
+areMapsEqual(map1, map2);

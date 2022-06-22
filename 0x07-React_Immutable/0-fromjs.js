2f57c6cadfc4b3c0389da6bab6aef6510c02874d
tieje <toj320@gmail.com>
2022-06-22 10:58:07 -0400

    wc: work in progress

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -3,6 +3,6 @@ import { fromJS } from "./node_modules/immutable/dist/immutable"
 function getImmutableObject(object) { return (fromJS(object)) }
 export default getImmutableObject
 */
-const { fromJS } = require('immutable')
-function getImmutableObject(object) { return (fromJS(object)) }
-module.exports = getImmutableObject
+const { fromJS } = require('immutable');
+function getImmutableObject(object) { return (fromJS(object)) };
+module.exports = getImmutableObject;
